2ffa28343dece32a9db62c0034772b1f9bb93d09
n4chh <iortego42madrid@gmail.com>
2026-04-05 11:42:08 +0200

    Github status of current pane and current pane dir added to tmux status

--- themes/minimal/right_status.sh
+++ themes/minimal/right_status.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 cd $1
+PANE_PATH=$2
 source $PWD/colors.sh
 # LEFT_ICON=""
 # RIGHT_ICON=""
@@ -23,10 +24,56 @@ function vpn_status() {
     fi
 }
 
+git_status() {
+    local pane_path="$1"
+    local git_branch=$(git -C "$pane_path" rev-parse --abbrev-ref HEAD 2>/dev/null)
+    
+    # Build the detail string
+
+    echo -n " "
+    echo -n "#[fg=$TAGBG]"
+    echo -n "$LEFT_ICON"
+    echo -n "#[fg=$TAGFG bg=$TAGBG]"
+    echo -n "#[fg=$ACCENT bg=$TAGBG] "
+    echo -n "#[fg=$TAGFG bg=$TAGBG bold]"
+	echo -n "$pane_path"
+	if [ -n "$git_branch" ]; then
+    	echo -n "  #[fg=$TAGFG]$git_branch"
+    	# Ahead / behind
+    	local ahead=$(git -C "$pane_path" rev-list --count @{u}..HEAD 2>/dev/null)
+    	local behind=$(git -C "$pane_path" rev-list --count HEAD..@{u} 2>/dev/null)
+
+    	# Staged / unstaged / deleted
+    	local staged=$(git -C "$pane_path" diff --cached --numstat 2>/dev/null | wc -l | tr -d ' ')
+    	local unstaged=$(git -C "$pane_path" diff --numstat 2>/dev/null | wc -l | tr -d ' ')
+    	local deleted=$(git -C "$pane_path" ls-files --deleted 2>/dev/null | wc -l | tr -d ' ')
+
+    	# Stash
+    	local stash=$(git -C "$pane_path" stash list 2>/dev/null | wc -l | tr -d ' ')
+    	local details=""
+    	[ "$staged" -gt 0 ]   && details+="#[fg=$GREEN]+$staged "
+    	[ "$unstaged" -gt 0 ] && details+="#[fg=$YELLOW]~$unstaged "
+    	[ "$deleted" -gt 0 ]  && details+="#[fg=$RED]-$deleted "
+    	[ "$stash" -gt 0 ]    && details+="#[fg=$SUBTEXT0]󰏗 $stash "
+    	[ "$ahead" -gt 0 ]    && details+="#[fg=$BLUE]󰁝 $ahead "
+    	[ "$behind" -gt 0 ]   && details+="#[fg=$RED]󰁅 $behind "
+    	[ -n "$details" ] && echo -n " ${details%* }" 
+	fi
+    echo -n "#[nobold fg=$TAGBG bg=terminal]"
+    echo -n "$RIGHT_ICON "
+}
+
 date=$(date +"%D %T")
 vpn=$(vpn_status)
 
-echo -n "#[fg=terminal bold]#S"
+
+echo -n "#[fg=terminal]"
+echo -n "$(git_status "$PANE_PATH")"
+echo -n " "
+echo -n "#[fg=$TAGBG]$LEFT_ICON"
+echo -n "#[bg=$TAGBG fg=$TAGFG]#W"
+echo -n "#[bg=terminal fg=$TAGBG]$RIGHT_ICON#[fg=terminal]"
+echo -n " at #[bold]#S"
 # echo -n "$vpn"
 echo -n " "
 echo -n "#[fg=$PRIMARY]"
